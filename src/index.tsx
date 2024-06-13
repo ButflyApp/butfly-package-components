a77cb00fabdbc2424eae8521236d614d00685689
Jose Augusto Megres de Oliveira <49933483+augustomegres@users.noreply.github.com>
2024-06-12 22:55:07 -0300

    feat: new dropdown menu

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import '@/output.css'
+import "@/output.css";
 import { Alert } from "@/components/Alert";
 import { Autocomplete } from "@/components/Autocomplete";
 import { Button } from "@/components/Button";
@@ -7,20 +7,64 @@ import { Input } from "@/components/Input";
 import { Ping } from "@/components/Ping";
 import { Label } from "@/components/Label";
 import { RadioGroup, RadioGroupItem } from "@/components/RadioGroup";
-import { Checkbox } from '@/components/Checkbox';
-import { Switch } from '@/components/Switch';
-import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectSeparator, SelectTrigger, SelectValue } from '@/components/Select';
+import { Checkbox } from "@/components/Checkbox";
+import { Switch } from "@/components/Switch";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/Select";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuCheckboxItem,
+  DropdownMenuRadioItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuGroup,
+  DropdownMenuPortal,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuRadioGroup,
+} from "@/components/DropdownMenu";
 
 const SelectComponents = {
-    Root: Select,
-    Content: SelectContent,
-    Group: SelectGroup,
-    Item: SelectItem,
-    Label: SelectLabel,
-    Separator: SelectSeparator,
-    Trigger: SelectTrigger,
-    Value: SelectValue,
-  }
+  Root: Select,
+  Content: SelectContent,
+  Group: SelectGroup,
+  Item: SelectItem,
+  Label: SelectLabel,
+  Separator: SelectSeparator,
+  Trigger: SelectTrigger,
+  Value: SelectValue,
+};
+
+const DropdownComponents = {
+  Root: DropdownMenu,
+  MenuTrigger: DropdownMenuTrigger,
+  MenuContent: DropdownMenuContent,
+  MenuItem: DropdownMenuItem,
+  MenuCheckboxItem: DropdownMenuCheckboxItem,
+  MenuRadioItem: DropdownMenuRadioItem,
+  MenuLabel: DropdownMenuLabel,
+  MenuSeparator: DropdownMenuSeparator,
+  MenuShortcut: DropdownMenuShortcut,
+  MenuGroup: DropdownMenuGroup,
+  MenuPortal: DropdownMenuPortal,
+  MenuSub: DropdownMenuSub,
+  MenuSubContent: DropdownMenuSubContent,
+  MenuSubTrigger: DropdownMenuSubTrigger,
+  MenuRadioGroup: DropdownMenuRadioGroup,
+};
 
 export {
   Alert,
@@ -34,5 +78,6 @@ export {
   RadioGroupItem,
   Checkbox,
   Switch,
-  SelectComponents as Select
+  SelectComponents as Select,
+  DropdownComponents as DropdownMenu,
 };
